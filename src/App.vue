8cd6999bc94773269fef91cc98299c26550dac23
haruzf <10240933+ahru@user.noreply.gitee.com>
2022-07-09 15:28:33 +0800

    登录组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app">app根组件</div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>登录组件</div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {};
+  },
+  // created () {},
+  // // mounted () {},
+  // computed: {},
+  // watch: {},
+  // methods: {}
+};
+</script>
+
+<style>
+</style>

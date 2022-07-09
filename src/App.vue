bd9778fa11e6d17b9d882fa4432dd0087baff1ef
haruzf <10240933+ahru@user.noreply.gitee.com>
2022-07-09 13:05:46 +0800

    梳理项目架构

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,5 @@
 <template>
-  <div id="app">
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
-
-    <img src="./assets/logo.png" />
-    <router-view />
-  </div>
+  <div id="app">app根组件</div>
 </template>
 
 <script>
@@ -21,12 +9,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>

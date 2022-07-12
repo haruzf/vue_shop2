d47992d1a11b4f12dbcd0e166a692f8d66b9f2bc
haruzf <10240933+ahru@user.noreply.gitee.com>
2022-07-12 20:57:21 +0800

    按需引入elementUI

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '@/assets/css/global.css'
 
 // 手动配置element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+import { Button, Form, FormItem, Input } from 'element-ui';
+// import { Form, FormItem } from 'element-ui';
+// import { Input } from 'element-ui';
+
 Vue.use(ElementUI)
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 
 Vue.config.productionTip = false
 

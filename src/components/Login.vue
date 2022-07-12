7e12b529e06dfb313f2bc1dc131cf5fa3b232cba
haruzf <10240933+ahru@user.noreply.gitee.com>
2022-07-12 22:41:18 +0800

    实现表单数据验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,16 +6,21 @@
         <img src="@/assets/logo.png" alt="" />
       </div>
       <!-- 登录区域 -->
-      <el-form :model="loginForm" label-width="0px" class="login_form">
+      <el-form
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户名 -->
-        <el-form-item>
+        <el-form-item prop="username">
           <el-input
             v-model="loginForm.username"
             prefix-icon="iconfont icon-user-fill"
           ></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
             v-model="loginForm.password"
             prefix-icon="iconfont icon-3702mima"
@@ -40,6 +45,26 @@ export default {
       loginForm: {
         username: "zs",
         password: "123",
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入登录名称", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请输入登录密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
       },
     };
   },

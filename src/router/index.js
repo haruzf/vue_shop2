8d227c1a8309a823585a10d0ceff0e73ea6d8ad5
haruzf <10240933+ahru@user.noreply.gitee.com>
2022-07-14 20:31:16 +0800

    路由导航守卫控制访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,19 @@ import Home from '@/components/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     routes: [
         { path: '/', redirect: "/login" },
         { path: '/login', component: Login },
         { path: '/home', component: Home }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next()
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
+})
+
+export default router

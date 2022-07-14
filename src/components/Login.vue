b1a34244a15f5c43366eef6a55c71cbb74329d19
haruzf <10240933+ahru@user.noreply.gitee.com>
2022-07-14 19:11:20 +0800

    实现登录预验证和配置axios发起登录请求

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -30,7 +30,7 @@
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
@@ -72,6 +72,14 @@ export default {
   methods: {
     resetLoginForm() {
       this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200) return console.log("登录失败");
+        console.log("登录成功");
+      });
     },
   },
   // created () {},
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import '@/assets/css/global.css'
 // 导入字体图标
 import '@/assets/fonts/iconfont.css'
 
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 // 手动配置element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'

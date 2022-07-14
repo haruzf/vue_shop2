456c70fddb78d145a851e4e1a0c9ef07beec22e1
haruzf <10240933+ahru@user.noreply.gitee.com>
2022-07-14 21:48:59 +0800

    美化主页的header区域

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,12 @@
 <template>
   <el-container class="home-container">
     <!-- 头部区域 -->
-    <el-header
-      >Header<el-button type="info" @click="logout">退出</el-button></el-header
+    <el-header>
+      <div>
+        <img src="@/assets/heima.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button></el-header
     >
     <!-- 页面主体区域 -->
     <el-container>
@@ -36,6 +40,19 @@ export default {
 
 .el-header {
   background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 
 .el-aside {
